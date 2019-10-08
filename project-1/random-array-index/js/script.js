29fa720baed555e0a3b07e156e081cf818b7bb6c
LeeVaughn <dlvprogramming@gmail.com>
2019-10-08 15:43:11 -0700

    completed randam-array-index warmup

--- project-1/random-array-index/js/script.js
+++ project-1/random-array-index/js/script.js
@@ -17,20 +17,20 @@ const scoreTwoElement = document.querySelector('#score-2');
 // YOUR CODE GOES HERE!!! Do the steps below to complete this challenge
 
 // 1) Uncomment the array below
-//const twelveSidedDie = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
+const twelveSidedDie = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
 // 2) Create a function named diceRoll.  Give it a parameter called array — this goes in the parens.
   // 2a) Create a variable that uses `Math.ceil`, `Math.random` and the length of the array parameter to generate a random number
   // NOTE: Introducing Functions video — https://teamtreehouse.com/library/introducing-functions-5
   // NOTE: Create a Random Number video — https://teamtreehouse.com/library/javascript-basics/working-with-numbers/create-a-random-number
-
   // 2b) `return` the random number variable
-
+  function diceRoll(array) {
+    const roll = Math.ceil(Math.random() * array.length);
+    
+    return roll;
+  }
 
 // 3) Complete the steps in the comments in the event listener below
-
-
 /**
  * Event listeners for playBtn - Write code in the body of the callback in the event listener below to complete this exercise
  */
@@ -40,13 +40,16 @@ playBtn.addEventListener('click', () => {
 
   // 4) Create two variables to store player one and player two scores
     // 4a) Set both variables equal to a call to the `diceRoll` function - Arguments: twelveSidedDie
-
+  const scorePlayer1 = diceRoll(twelveSidedDie);
+  const scorePlayer2 = diceRoll(twelveSidedDie);
 
   // 5) Log out the two variables above to test their values — check them out in the console
-
+  console.log(scorePlayer1);
+  console.log(scorePlayer2);
 
   // 6) Set the innerHTML of the `scoreOneElement` and `scoreTwoElement` variables above equal to the dice roll variables you just created
-
+  scoreOneElement.innerHTML = scorePlayer1;
+  scoreTwoElement.innerHTML = scorePlayer2;
 
   // Helpful log statement to test function — You can comment out this if you like
   console.log('Play button is functional!');
